4186b18dd34c62e43022f8aac0b823a4b55dc1d9
Vladyslav Tochanenko <itochanenko@gmail.com>
2022-12-04 20:25:58 +0200

    Add compressed and decompressed files generation

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,18 @@
 import java.io.File
+import java.lang.StringBuilder
 import java.nio.file.Paths
 
 val PATH = Paths.get("").toAbsolutePath().toString() + "\\src\\main\\resources\\"
 
 fun main() {
-//    val inputText = readFile(PATH + "to_encode.txt")
-//    writeFile(PATH + "encoded.txt", inputText)
+    val inputText = readFile(PATH + "to_encode.txt")
 
-    val compressed = lzwEncode("geekific-geekific")
-    println(compressed)
-    val decompressed = lzwDecode(compressed.toMutableList())
-    println(decompressed)
+    val compressed = lzwEncode(inputText)
+    writeFile(PATH + "compressed.txt", intToString(compressed))
+
+    val compressedText = readFile(PATH + "compressed.txt")
+    val decompressed = lzwDecode(stringToInt(compressedText))
+    writeFile(PATH + "decompressed.txt", decompressed)
 }
 
 fun readFile(path: String): String = File(path)
@@ -24,4 +26,20 @@ fun writeFile(path: String, text: String) {
     File(path).bufferedWriter().use { out ->
         out.write(text)
     }
+}
+
+fun intToString(intList: List<Int>): String {
+    val res = StringBuilder("")
+    for (i in intList) {
+        res.append(i.toChar())
+    }
+    return res.toString()
+}
+
+fun stringToInt(str: String): MutableList<Int> {
+    val res = mutableListOf<Int>()
+    for (i in str) {
+        res.add(i.code)
+    }
+    return res
 }
